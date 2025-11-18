176133a1e2bd256151a274874e5b033775cb79ec
Danny Han <dannyhan@going.cloud>
2025-11-18 12:12:08 +0800

    docs: Document HttpResponse

--- src/schema/http-response.ts
+++ src/schema/http-response.ts
@@ -1,6 +1,8 @@
 /**
- * @description response from lambda
- * @ref https://docs.aws.amazon.com/apigateway/latest/developerguide/api-gateway-integration-settings-integration-response.html
+ * @description Response from lambda. Should be auto inferred when on API Gateway payloadFormatVersion is "2.0"
+ *
+ * @see {@link https://docs.aws.amazon.com/apigateway/latest/developerguide/http-api-develop-integrations-lambda.html#http-api-develop-integrations-lambda.proxy-format | AWS API Getaway proxy integration documentation}
+ * @see {@link https://docs.aws.amazon.com/apigateway/latest/developerguide/api-gateway-integration-settings-integration-response.html | AWS API Gateway non proxy documentation}
  */
 export type HttpResponse = {
   isBase64Encoded: boolean;
